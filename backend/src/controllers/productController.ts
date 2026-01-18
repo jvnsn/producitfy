44615613a5243bc4396a5fe8614cacd917c60132
jvnsn <joviansenwidyanto@google.com>
2026-01-18 16:35:08 +0800

    Implement comments feature with creation and deletion; enhance product page and profile management

--- backend/src/controllers/productController.ts
+++ backend/src/controllers/productController.ts
@@ -31,6 +31,7 @@ export const getProductById = async (req: Request, res: Response) => {
         const product = await queries.getProductById(id as string);
 
         if (!product) return res.status(404).json({ error: "Product not found "});
+        res.status(200).json(product);
     } catch (error) {
         console.error("Error getting product:", error);
         res.status(500).json({ error: "Failed to get product" });
