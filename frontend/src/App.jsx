4f96bfdb54882b7a90a4cbedda09f633ca1c945e
jvnsn <joviansenwidyanto@google.com>
2026-01-18 15:07:01 +0800

    Add product creation and loading components; implement product listing with error handling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,9 +8,11 @@ import CreatePage from "./pages/CreatePage"
 import EditProductPage from "./pages/EditProductPage"
 import useAuthReq from "./hooks/useAuthReq"
 import useUserSync from "./hooks/useUserSync"
+import { Navigate } from "react-router"
+
 
 function App() {
-  const { isClerkLoaded } = useAuthReq()
+  const { isClerkLoaded, isSignedIn } = useAuthReq()
   useUserSync()
 
   if (!isClerkLoaded) return null;
@@ -22,9 +24,9 @@ function App() {
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/product/:id" element={<ProductPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/create" element={<CreatePage />} />
-            <Route path="/edit/:id" element={<EditProductPage />} />
+            <Route path="/profile" element={isSignedIn ? <ProfilePage /> : <Navigate to={"/"} />} />
+            <Route path="/create" element={isSignedIn ? <CreatePage /> : <Navigate to={"/"} />} />
+            <Route path="/edit/:id" element={isSignedIn ? <EditProductPage /> : <Navigate to={"/"} /> } />
         </Routes>
       </main>
     </div>
